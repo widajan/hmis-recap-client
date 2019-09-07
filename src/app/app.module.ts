b3537843342a7a8bff729daa3b95287a0fd3ddc3
Wida Bahar <widabahar4@gmail.com>
2019-09-07 16:08:45 +0430

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './hmis/department/department.component';
+import { DoctorComponent } from './hmis/doctor/doctor.component';
+import { HospitalComponent } from './hmis/hospital/hospital.component';
+import { PatientComponent } from './hmis/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    DoctorComponent,
+    HospitalComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
